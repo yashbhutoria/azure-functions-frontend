49c34f57b504ca9a76d0b284d12d315ee6737a87
Yash Bhutoria <Yash.Bhutoria@welkgroup.com>
2020-02-06 15:50:40 +0530

    Resolved import error

--- frontend_helper/__init__.py
+++ frontend_helper/__init__.py
@@ -1,11 +1,8 @@
-import pathlib
 from jinja2 import Template
-
-def sibling_path(name):
-    return pathlib.Path(__file__).parent / name
+import azure.functions as func
 
 def render_template(template_name,**kwargs):
-    with open(sibling_path(f"templates/{template_name}")) as f:
+    with open(f"templates/{template_name}") as f:
         template = Template(f.read())
         return func.HttpResponse(
             template.render(**kwargs),
--- index/__init__.py
+++ index/__init__.py
@@ -1,11 +1,7 @@
 import logging
 import azure.functions as func
-# from azfuncfrontendlib import render_template
+from __app__.frontend_helper import render_template
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    return func.HttpResponse(
-        "Frontend"
-    )
-    # return render_template('index.html',name = 'Jinja')    
-    
+    return render_template('index.html',name = 'Jinja')
